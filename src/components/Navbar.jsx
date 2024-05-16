a7315fdce4bc45b49a6770d3163f8987cdceda33
JohaneDecamps <johane.decamps@gmail.com>
2024-05-16 15:11:49 +0200

    fix routes and App.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-export default function Navbar () 
+export default function Navbar () {
 
 return (
-    
+    <> </>
 )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import HomePage from "./pages/HomePage.jsx"
+import PlantesPage from "./pages/PlantesPage.jsx"
+import RandoPage from "./pages/RandoPage.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter ([
+  {
+    element: <App/>,
+    children: [
+      {
+        path : "/", 
+        element : <HomePage/>
+      },
+      {
+        path : "/plantes", 
+        element : <PlantesPage/>
+      },
+      {
+        path : "/rando", 
+        element : <RandoPage/>
+      },
+
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
