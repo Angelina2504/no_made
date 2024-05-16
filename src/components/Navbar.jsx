d4f485533d864692b7a40b08bf78247b876b1ebc
JohaneDecamps <johane.decamps@gmail.com>
2024-05-16 23:36:03 +0200

    css in page 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
+import style from "../styles/navbar.module.css"
+import { Link } from "react-router-dom";
+import logo from "../../public/logos/logo.png";
+
 export default function Navbar() {
   return (
-    <>
-   
-    </>
+    <nav className={style.navbar}>
+      <img className={style.logo} src={logo}/>
+      <ul className={style["nav-list"]}>
+       <Link className={style.link}to="/rando"><li>Randonnées</li> </Link>
+       <Link className={style.link} to="/plantes"> <li>Les plantes</li> </Link>
+      </ul>
+    </nav>
   );
 }
