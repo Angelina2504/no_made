47fff9abff4c82cf42b48454126a45843e6dbf54
JohaneDecamps <johane.decamps@gmail.com>
2024-05-17 10:29:39 +0200

    more css

--- src/components/CardListPlantes.jsx
+++ src/components/CardListPlantes.jsx
@@ -7,32 +7,27 @@ import imag from "../../public/imagesPlantes/tree.jpg";
 export default function CardListPlantes() {
   return (
     <>
-    <article className={style.allPage}> 
-  
+      <article className={style.allPage}>
         <img className={style.imgArbre} src={imag} />
-   
-      {/* <div className={style.citationUn}>Lorem ipsum Lorem ipsum</div>
-      <div className={style.citationDeux}>Lorem ipsum Lorem ipsum</div> */}
-      <p className={style.titrePlante}>
-        {" "}
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae,
-        ad. Dicta consequuntur aspernatur beatae tempore accusantium nostrum
-        fugiat voluptatem! Doloribus aliquam fugiat eligendi quam reprehenderit
-        laboriosam voluptates sed dignissimos perferendis?
-      </p>
-      <div className={style.AllCardPlant}>
-        {plantes.map((p) => (
-          <CardsPlantes plante={p} key={p.id} />
-        ))}
-      </div>
 
-      <div className={style.containerButton}>
-        <Link to="/rando" className={style.buttonSuivant}>
-          <button className={style.button}>
-            <h2>Partir à l'aventure</h2>
-          </button>
-        </Link>
-      </div></article>
+        <p className={style.titrePlante}>
+          "Je me suis promené dans les bois et je suis sorti plus grand que les
+          arbres."
+        </p>
+        <div className={style.AllCardPlant}>
+          {plantes.map((p) => (
+            <CardsPlantes plante={p} key={p.id} />
+          ))}
+        </div>
+
+        <div className={style.containerButton}>
+          <Link to="/rando" className={style.buttonSuivant}>
+            <button className={style.button}>
+              <h2>Partir à l'aventure</h2>
+            </button>
+          </Link>
+        </div>
+      </article>
     </>
   );
 }
--- src/components/CardListRando.jsx
+++ src/components/CardListRando.jsx
@@ -2,28 +2,31 @@ import randos from "../assets/data/randonnee.json";
 import CardsRando from "../components/CardsRando";
 import { Link } from "react-router-dom";
 import style from "../styles/cardlistrando.module.css";
+import bg from "../../public/imagesRandos/bgRando.jpg"
 
 export default function CardListRando() {
   return (
     <>
-      {/* <div className={style.citation}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium officiis maiores, obcaecati facilis animi quod non qui laboriosam voluptatum ratione dolores, quisquam ducimus enim eius. Earum accusantium officiis praesentium numquam? </div> */}
-
+       <img className={style.imgRando} src={bg} />
       <p className={style.titreRando}>
-     Une selection de randonnées a faire en France 
+      Telle un défi à la vitesse et au bruit, la marche incite à la modestie, pousse à la curiosité, suscite la méditation. Elle invite au repli, à l’intimité, à se taire pour mieux écouter.  - Franck Michel
+      </p>
+      <p className={style.presentation}>
+      Une sélection de plusieurs randonnées pour vous évader
       </p>
       <div className={style.AllCardRando}>
         {randos.map((p) => (
           <CardsRando rando={p} key={p.id} />
         ))}
       </div>
-
+{/* 
       <div className={style.containerButton}>
         <Link to="/plante" className={style.buttonSuivant}>
           <button className={style.button}>
             <h2>???</h2>
           </button>
         </Link>
-      </div>
+      </div> */}
     </>
   );
 }
