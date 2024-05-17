e236fbf1b6a0bfc632c28bff1504ed36dfa84eb0
JohaneDecamps <johane.decamps@gmail.com>
2024-05-17 08:44:14 +0200

    css sur les trois pages

--- src/components/CardListPlantes.jsx
+++ src/components/CardListPlantes.jsx
@@ -1,17 +1,18 @@
 import plantes from "../assets/data/plantes.json";
 import CardsPlantes from "../components/CardsPlantes";
 import { Link } from "react-router-dom";
-import style from "../styles/cardlist.module.css";
+import style from "../styles/cardlistplante.module.css";
 import imag from "../../public/imagesPlantes/tree.jpg";
 
 export default function CardListPlantes() {
   return (
     <>
+    <article className={style.allPage}> 
   
         <img className={style.imgArbre} src={imag} />
    
-      <div className={style.citations}>Lorem ipsum Lorem ipsum</div>
-      {/* <div className={style.citationDeux}>Lorem ipsum Lorem ipsum</div> */}
+      {/* <div className={style.citationUn}>Lorem ipsum Lorem ipsum</div>
+      <div className={style.citationDeux}>Lorem ipsum Lorem ipsum</div> */}
       <p className={style.titrePlante}>
         {" "}
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae,
@@ -31,7 +32,7 @@ export default function CardListPlantes() {
             <h2>Partir à l'aventure</h2>
           </button>
         </Link>
-      </div>
+      </div></article>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import logo from "../../public/logos/logo.png";
 export default function Navbar() {
   return (
     <nav className={style.navbar}>
-      <img className={style.logo} src={logo}/>
+      <Link to ="/"> <img className={style.logo} src={logo}/></Link>
       <ul className={style["nav-list"]}>
-       <Link className={style.link}to="/rando"><li>Randonnées</li> </Link>
-       <Link className={style.link} to="/plantes"> <li>Les plantes</li> </Link>
+      <Link className={style.link} to="/plantes"> <li>Les plantes</li> </Link>
+      <Link className={style.link}to="/rando"><li>Randonnées</li> </Link>
       </ul>
     </nav>
   );
