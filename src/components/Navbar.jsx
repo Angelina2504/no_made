2c066a61f4bbaa4dbbfa7c7d42dd9f7b343087f6
Angélina Droz <angieso91@gmail.com>
2024-06-13 22:17:39 +0200

    fix pictures

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import style from "../styles/navbar.module.css"
 import { Link } from "react-router-dom";
-import logo from "../../public/logos/logo.png";
+import logo from "/logos/logo.png";
 
 export default function Navbar() {
   return (
